6ac9797ecca2432d94b1b5860641a7c1a78c80da
Aniket Walunj <86148992+dhanooanky@users.noreply.github.com>
2022-01-30 21:30:46 +0530

    Add files via upload

---
+++ live test.py
@@ -0,0 +1,34 @@
+nums=[11,7,2,15]
+
+
+
+class solution:
+    def countElements(self,nums:List[int])
+    M=max(nums)
+    m=min(nums)
+    return sum(1 for in nums if m<i<M)
+
+
+groups =[[1,-1,-1],[3,-2,0]],  nums = [1,-1,0,1,-1,-1,3,-2,0]
+class Solution:
+    def aniket(self,groups:List[list(int)],nums:List[int])
+    def biuld(groups):
+        n=len(group)
+        lps=[0]*n
+        j=0
+        for i in range(1,n):
+            while j>0 and group[i] !=group[j]:
+                j=lps(j-1)
+                if group[i]==group[j]:
+                    lps(i)
+                    j+=1
+                    
+                    return lps 
+                def anky(groupindex,numIndex):
+                    if groupIndex ==n:
+                        return True
+                    if numIndex ==m:
+                        return False
+                 
+            
+        
